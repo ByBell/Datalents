6d8cc6a22e1ab165b5e578ecd7ba19a5f7c65bff
Brendan <brendan.poirier@hetic.net>
2018-02-09 11:47:13 +0100

    Added AuthController and LoginAuthenticator

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $helper = $this->get('security.authentication_utils');
+
+        return $this->render(
+            'auth/login.html.twig',
+            array(
+                'last_username' => $helper->getLastUsername(),
+                'error'         => $helper->getLastAuthenticationError(),
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="security_login_check")
+     */
+    public function loginCheckAction()
+    {
+
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request)
+    {
+        // Create a new blank user and process the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encode the new users password
+            $encoder = $this->get('security.password_encoder');
+            $password = $encoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // Set their role
+            $user->setRole('ROLE_USER');
+
+            // Save
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('auth/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
